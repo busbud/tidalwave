8b23f37250930fdae11af47cc0330984e8866738
Dustin Blackman <dev@dustinblackman.com>
2018-09-24 10:10:33 -0400

    fix max cpu

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -22,9 +22,9 @@ func maxParallelism() int {
 	maxProcs := runtime.GOMAXPROCS(0)
 	numCPU := runtime.NumCPU()
 	if maxProcs < numCPU {
-		return maxProcs - 1
+		return maxProcs
 	}
-	return numCPU - 1
+	return numCPU
 }
 
 func cliQuery() {
